6c12c3cb583a714d2b7b8d42b665e455920613af
Anusorn <anusorn.lt2006@gmail.com>
2025-09-24 21:36:49 +0700

    Update meteoriteThread.java
    เพิ่มความเร็วชนขอบ, ตอนเกิดใหม่หลายตัวไม่เร็ว

--- Meteorite/src/meteoriteThread.java
+++ Meteorite/src/meteoriteThread.java
@@ -3,40 +3,50 @@ import javax.swing.*;
 class meteoriteThread extends Thread {
     JLabel label;
     JPanel panel;
-    int dx, dy;
+    double dx, dy, initDx, initDy;
+    double posX, posY;
+   
+    
 
-    meteoriteThread(JLabel label, JPanel panel, int dx, int dy) {
+    meteoriteThread(JLabel label, JPanel panel, double dx, double dy) {
         this.label = label;
         this.panel = panel;
         this.dx = dx;
         this.dy = dy;
+        this.initDx = dx;  
+        this.initDy = dy;
+        this.posX = label.getX();
+        this.posY = label.getY();
     }
 
     @Override
     public void run() {
+        
         while (true) {
-            int newX = label.getX() + dx;
-            int newY = label.getY() + dy;
 
-            if (newX <= 0 || newX >= panel.getWidth() - label.getWidth()) {
+            posX += dx;
+            posY += dy;
+
+            if (posX <= 0) {
                 dx = -dx;
-                if(dx > 0){
-                    dx += 1;
-                }else{
-                    dx -= 1;
-                }
+                dx += 1;
             }
-            if (newY <= 0 || newY >= panel.getHeight() - label.getHeight()) {
+            if (posX >= panel.getWidth() - label.getWidth()) {
+                dx = -dx;
+                dx -= 1;
+
+            }
+
+            if (posY <= 0) {
                 dy = -dy;
-                if(dy > 0){
-                   dy += 1; 
-                }else {
-                    dy -= 1;
-                }
+                dy += 1;
+            }
+            if (posY >= panel.getHeight() - label.getHeight()) {
+                dy = -dy;
+                dy -= 1;
             }
 
-            label.setLocation(label.getX() + dx, label.getY() + dy);
-
+            label.setLocation((int) posX, (int) posY);
             try {
                 Thread.sleep(16);
             } catch (InterruptedException e) {
