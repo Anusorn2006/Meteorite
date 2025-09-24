8a3f840264284a013c3998fd11775c7caef289da
TsumiyaZ <watcharakornnigc@gmail.com>
2025-09-24 21:54:34 +0700

    Constants

--- Meteorite/src/PanelMeteorite.java
+++ Meteorite/src/PanelMeteorite.java
@@ -3,31 +3,26 @@ import java.awt.*;
 import java.util.Random;
 import java.net.URL;
 
-class PanelMeteorite extends JFrame {
+        class PanelMeteorite extends JFrame {
 
-    int amountMeteorite = 100;
-    JLabel[] meteorite = new JLabel[amountMeteorite];
-    meteoriteThread[] mtoT = new meteoriteThread[amountMeteorite];
-    ImageIcon[] mtoIcon = new ImageIcon[amountMeteorite];
-    Random rand = new Random();
+            int amountMeteorite = 50;
+            JLabel[] meteorite = new JLabel[amountMeteorite];
+            meteoriteThread[] mtoT = new meteoriteThread[amountMeteorite];
+            ImageIcon[] mtoIcon = new ImageIcon[amountMeteorite];
+            Random rand = new Random();
 
-    PanelMeteorite() {
-        setSize(600, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+            PanelMeteorite() {
+                setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setLocationRelativeTo(null);
 
-        JPanel BackG = new JPanel(null);
-        BackG.setBackground(Color.BLACK);
+                JPanel BackG = new JPanel(null);
+                BackG.setBackground(Color.BLACK);
 
         String[] imageFiles = {"meteorite2.png", "meteorite3.png", "meteorite4.png", "meteorite5.png", "meteorite6.png"};
 
         for (int i = 0; i < meteorite.length; i++) {
             String chosenFile = imageFiles[rand.nextInt(imageFiles.length)];
-
-            // โหลดรูปตรง ๆ
-//            ImageIcon rawIcon = new ImageIcon(chosenFile);
-//            Image scaled = rawIcon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-//            mtoIcon[i] = new ImageIcon(scaled);
 
             URL url = PanelMeteorite.class.getResource("/" + chosenFile); // ไฟล์อยู่ใน classpath root
             ImageIcon rawIcon = new ImageIcon(url);
